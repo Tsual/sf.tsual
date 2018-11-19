4405cafa9ad8b75cff2074bc58ead7b2281761cd
Tsual <562974055@qq.com>
2018-11-19 21:17:42 +0800

    tboot - release

--- NgTsual/src/main/java/sf/tboot/BootGr.java
+++ NgTsual/src/main/java/sf/tboot/BootGr.java
@@ -2,19 +2,24 @@ package sf.tboot;
 
 public class BootGr
 {
-	public static void main(String[] args)
+	public static void main(String[] args) throws ClassNotFoundException
 	{
-		String nr = "\r\n"; //回车
-		String source = "package sf.temp; " + nr +
-				" public class  Hallo{" + nr +
-				" public static void main (String[] args){" + nr +
-				" System.out.println(\"HelloWorld! 1\");" + nr +
-				" }" + nr +
-				" }";
-		final TbootClassLoader tbootClassLoader = new TbootClassLoader();
-		tbootClassLoader.register("sf.temp.Hallo", source);
-		Thread thread = Thread.currentThread();
-		thread.setContextClassLoader(tbootClassLoader);
+		String source = "package sf.tboot.temp;\n" +
+				"\n" +
+				"import sf.uds.util.StringHelper;\n" +
+				"\n" +
+				"import java.util.Arrays;\n" +
+				"\n" +
+				"public class Fdsfff\n" +
+				"{\n" +
+				"\tpublic static void main(String[] args)\n" +
+				"\t{\n" +
+				"\t\tif (StringHelper.isNotEmpty(\" ff\"))\n" +
+				"\t\t\tSystem.out.println(Arrays.asList(\"a\", \"b\"));\n" +
+				"\t}\n" +
+				"}\n";
+		TBootstrapper.load("sf.tboot.temp.Fdsfff", source);
+		final Class<?> aClass = Class.forName("sf.tboot.temp.Fdsfff");
 
 		int a = 0;
 	}
---
+++ NgTsual/src/main/java/sf/tboot/TBootstrapper.java
@@ -0,0 +1,120 @@
+package sf.tboot;
+
+import javax.tools.*;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.OutputStream;
+import java.lang.reflect.Method;
+import java.net.URI;
+import java.nio.Buffer;
+import java.security.AccessController;
+import java.security.PrivilegedAction;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class TBootstrapper
+{
+	public static Class<?> load(String fullClassName, File javaClassFile)
+	{
+		final int BUFFER_SIZE = 1024;
+		try {
+			if (javaClassFile != null && javaClassFile.isFile()) {
+				final ArrayList<byte[]> list = new ArrayList<>();
+				try (FileInputStream fis = new FileInputStream(javaClassFile)) {
+					int read_count;
+					byte[] buffer = new byte[BUFFER_SIZE];
+					while (BUFFER_SIZE == (read_count = fis.read(buffer))) {
+						list.add(buffer);
+						buffer = new byte[BUFFER_SIZE];
+					}
+
+					byte[] data = new byte[BUFFER_SIZE * list.size() + read_count];
+					for (int i = 0; i < list.size(); i++)
+						System.arraycopy(list.get(i), 0, data, BUFFER_SIZE * i, BUFFER_SIZE);
+					if (read_count >= 0) System.arraycopy(buffer, 0, data, BUFFER_SIZE * list.size(), read_count);
+					return load(fullClassName, data);
+				}
+			} else {
+				return null;
+			}
+		} catch (Throwable e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+
+	public static Class<?> load(String fullClassName, byte[] javaClassByteArray)
+	{
+		if (javaClassByteArray != null) {
+			final ClassLoader systemClassLoader = ClassLoader.getSystemClassLoader();
+			return AccessController.doPrivileged((PrivilegedAction<Class>) () ->
+			{
+				try {
+					final Method defineClass = ClassLoader.class.getDeclaredMethod("defineClass", String.class, byte[].class, int.class, int.class);
+					defineClass.setAccessible(true);
+					return (Class) defineClass.invoke(systemClassLoader, fullClassName, javaClassByteArray, 0, javaClassByteArray.length);
+				} catch (Throwable e) {
+					throw new RuntimeException(e);
+				}
+			});
+		} else {
+			return null;
+		}
+	}
+
+	public static Class<?> load(String fullClassName, String javaCode)
+	{
+		try (ByteArrayOutputStream bos = new ByteArrayOutputStream()) {
+			final JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+
+			final JavaFileManager file_manager = new ForwardingJavaFileManager(compiler.getStandardFileManager(null, null, null))
+			{
+				@Override
+				public JavaFileObject getJavaFileForOutput(Location location, String className, JavaFileObject.Kind kind, FileObject sibling)
+				{
+					final URI class_uri = URI.create("string:///" + className.replace('.', '/') + kind.extension);
+					return new SimpleJavaFileObject(class_uri, kind)
+					{
+						@Override
+						public OutputStream openOutputStream()
+						{
+							return bos;
+						}
+					};
+				}
+			};
+
+			final List<String> compile_param = Arrays.asList("-encoding", "UTF-8");
+
+			final URI source_uri = URI.create("string:///" + fullClassName.replace('.', '/') + JavaFileObject.Kind.SOURCE.extension);
+			final JavaFileObject source = new SimpleJavaFileObject(source_uri, JavaFileObject.Kind.SOURCE)
+			{
+				@Override
+				public CharSequence getCharContent(boolean ignoreEncodingErrors)
+				{
+					return javaCode;
+				}
+			};
+
+			if (compiler.getTask(null, file_manager, null, compile_param, null, Collections.singletonList(source)).call()) {
+				return load(fullClassName, bos.toByteArray());
+			} else {
+				return null;
+			}
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public static Class<?> load(String fullClassName, Buffer javaCodeNioBuffer)
+	{
+		if (javaCodeNioBuffer.hasArray())
+			return load(fullClassName, (byte[]) javaCodeNioBuffer.array());
+		else return null;
+	}
+
+
+}
