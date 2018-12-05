0480e1f543e08a00b76522d4942a5eb512d632d6
张良 <562974055@qq.com>
2018-12-05 11:52:32 +0800

    stringHelper

--- NgTsual/src/main/java/sf/tquery/common/Iterators.java
+++ NgTsual/src/main/java/sf/tquery/common/Iterators.java
@@ -4,12 +4,12 @@ import sf.uds.util.ObjectHelper;
 
 public class Iterators
 {
-	public static <T> Iterator<T> basic(java.lang.Iterable<T> it)
+	public static <T> Iterator<T> get(java.lang.Iterable<T> it)
 	{
 		return new BasicIterator<T>(ObjectHelper.requireNotNull(it));
 	}
 
-	public static <T> Iterator<T> basic(T[] ar)
+	public static <T> Iterator<T> get(T[] ar)
 	{
 		return new BasicIterator<T>(ObjectHelper.requireNotNull(ar));
 	}
--- NgTsual/src/main/java/sf/uds/util/StringHelper.java
+++ NgTsual/src/main/java/sf/uds/util/StringHelper.java
@@ -1,5 +1,8 @@
 package sf.uds.util;
 
+import sf.tquery.common.Iterators;
+import sf.uds.interfaces.del.executable.IExec_1;
+
 public class StringHelper
 {
 	public static boolean isEmpty(String str)
@@ -32,12 +35,45 @@ public class StringHelper
 		return isEmpty(str) ? str : Character.toLowerCase(str.charAt(0)) + str.substring(1);
 	}
 
+	public static <T> String join(T[] objs, String jspt, final IExec_1<String, T> exec)
+	{
+		try {
+			return join(Iterators.get(objs).as(exec::execute).toList(), jspt);
+		} catch (Exception e) {
+			return join(objs, jspt);
+		}
+	}
+
 	public static String join(Object[] objs, String jspt)
 	{
 		StringBuilder res = new StringBuilder(objs.length > 0 ? objs[0].toString() : "");
-		for (int i = 1; i < objs.length; i++) {
+		for (int i = 1; i < objs.length; i++)
 			res.append(jspt).append(objs[i].toString());
-		}
 		return res.toString();
+	}
+
+	public static <T> String join(java.lang.Iterable<T> objs, String jspt, final IExec_1<String, T> exec)
+	{
+		try {
+			return join(Iterators.get(objs).as(exec::execute).toList(), jspt);
+		} catch (Exception e) {
+			return join(objs, jspt);
+		}
+	}
+
+	public static String join(java.lang.Iterable objs, String jspt)
+	{
+		StringBuilder sb = new StringBuilder();
+		for (Object o : objs)
+			sb.append(jspt).append(o);
+		return sb.toString().substring(jspt.length());
+	}
+
+	public static String minimize(String str)
+	{
+		return str.replaceAll(" ", "")
+				.replaceAll("\n", "")
+				.replaceAll("  ", "")
+				.replaceAll("\t", "");
 	}
 }
