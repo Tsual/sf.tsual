808b8921f8a92ef5676cc6c0b07e9c5c7f029067
张良 <562974055@qq.com>
2018-12-05 15:43:42 +0800

    submit

--- NgTsual/src/main/java/sf/helper/IllegalArgumentExceptionHelper.java
+++ NgTsual/src/main/java/sf/helper/IllegalArgumentExceptionHelper.java
@@ -1,13 +1,12 @@
 package sf.helper;
 
-import com.sun.istack.internal.NotNull;
 import sf.uds.util.StringHelper;
 
 import java.lang.reflect.Method;
 
 public class IllegalArgumentExceptionHelper
 {
-	public static String getFullTraceInfoEn(@NotNull IllegalArgumentException ex, @NotNull Method method, Object[] args)
+	public static String getFullTraceInfoEn(IllegalArgumentException ex, Method method, Object[] args)
 	{
 		StringBuilder sb = new StringBuilder(ex.getMessage()).append("\nReflect information\n");
 		for (Class method_arg_class : method.getParameterTypes())
