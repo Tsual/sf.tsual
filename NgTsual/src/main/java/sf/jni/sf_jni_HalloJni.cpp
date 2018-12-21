17581f116f3796653b2f8ef1a3a921b6c79d8d80
张良 <562974055@qq.com>
2018-12-21 15:00:32 +0800

    cpp单独移动到一个目录

--- NgTsual/src/main/java/sf/jni/sf_jni_HalloJni.cpp
+++
@@ -1,5 +0,0 @@
-#include "sf_jni_HalloJni.h"
-
-JNIEXPORT jbyteArray JNICALL Java_sf_jni_HalloJni_foo(JNIEnv *, jclass, jbyteArray, jstring, jint, jobject)
-{
-}
---
+++ jni/sf_jni_HalloJni.cpp
@@ -0,0 +1,6 @@
+#include "sf_jni_HalloJni.h"
+
+JNIEXPORT jbyteArray JNICALL Java_sf_jni_HalloJni_foo(JNIEnv *, jclass var0, jbyteArray var1, jstring var2, jint var3, jobject var4)
+{
+    return var1;
+}
