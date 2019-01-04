4d13a07226877b5ccb0bf1d4bfdd164b76851c9d
Tsual <562974055@qq.com>
2019-01-04 16:14:35 +0800

    submit

--- NgTsual/src/test/java/unit/NativeAesCtrTest.java
+++ NgTsual/src/test/java/unit/NativeAesCtrTest.java
@@ -24,21 +24,21 @@ public class NativeAesCtrTest {
     @Test
     public void performance() throws Exception {
         int count = 100;
-        final byte[] bytes = StringHelper.randomString(50).getBytes();
+        final byte[] bytes = StringHelper.randomString(5000).getBytes();
         final String key = StringHelper.randomString(20);
         final NativeAesCtr nativeAesCtr = new NativeAesCtr(key);
         final SimpleAesHelper simpleAesHelper = new SimpleAesHelper(key.getBytes());
 
-        int res = 0;
+        long res = 0;
         while (count-- > 0) {
             final long l1 = System.nanoTime();
             nativeAesCtr.encrypt(bytes);
             final long l2 = System.nanoTime();
             simpleAesHelper.encrypt(bytes);
             final long l3 = System.nanoTime();
-            res += (l2 - l1 > l3 - l2 ? -1 : 1);
+            res += (l3 - l2 - l2 + l1);
         }
         if (res < 0)
-            Assert.fail();
+            Assert.fail(String.valueOf(res));
     }
 }
